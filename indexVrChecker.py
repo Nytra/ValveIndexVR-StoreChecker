66f898179fca830dea0adbc0a56aabc84a9ae152
Sam <samueltscott@gmail.com>
2021-07-18 14:18:24 +0100

    Improved comments and code formatting
    - Cleaned up source code
    
    - Removed superfluous comments
    
    - Added new comments for better explanation of code

--- indexVrChecker.py
+++ indexVrChecker.py
@@ -1,9 +1,9 @@
-# program to check if the Valve Index line of VR products are available on Steam
+# program to check if the Valve Index VR products are available on Steam
 
 #   command-line args:
-#       --nopause : don't pause before program exit
-#       --noalarm : don't sound the alarm
-#       --checkall : check all sku purchase_options
+#     --nopause : don't pause before program exit
+#     --noalarm : don't sound the alarm
+#     --checkall : check all sku purchase_options
 
 from bs4 import BeautifulSoup
 import requests
@@ -27,6 +27,7 @@ SKU_STRINGS = [
 ]
 
 # ===== DEFINE WHAT YOU WANT IN THE LIST BELOW =====
+
 # (THESE MUST BE NUMBERS) (Uncomment the ones you want)
 
 WANTED_ITEMS = [
@@ -45,7 +46,6 @@ def check_arg(arg):
     if len(argv) > 1 and arg in argv[1:]:
         return True
     return False
-    
 
 # Get path to script directory
 scriptpath = os.path.realpath(__file__)
@@ -60,8 +60,6 @@ if not check_arg('--noalarm'):
         alert_sound_1 = None
 else:
     alert_sound_1 = None
-
-# === End of Globals and constants ===
 
 # === Utility functions ===
 
@@ -84,8 +82,6 @@ def check_available(btn_html, item_name):
         print(item_name, 'unknown status.')
         return -1
 
-# === End of Utility functions ===
-
 # === Main function ===
 
 def main():
@@ -93,36 +89,41 @@ def main():
 
     soup = BeautifulSoup(r.content, 'html.parser')
 
-    # stores sku purchase_options as BeautifulSoup selected objects (ResultSet object)
+    # stores sku purchase options as BeautifulSoup selected objects (ResultSet object)
     sku_arr = []
 
-    # select the div relating to the sku option. ex: 'div.valveindex_purchase_option.sku_full_kit'
+    # select the div relating to the sku purchase option - ex: 'div.valveindex_purchase_option.sku_full_kit'
     for i in range(len(SKU_STRINGS)):
         purchase_option = soup.select('div.valveindex_purchase_option.' + SKU_STRINGS[i])
+
+        # check that it contains something
         if purchase_option:
             sku_arr.append(purchase_option)
 
     any_available = False
     items_checked = 0
 
-    # for each purchase_option in sku_arr
+    # loop for each item in sku_arr
     for i in range(len(sku_arr)):
 
-        # select the 'Add to Cart' button
+        # select the 'Add to Cart' button from HTML (must select from [0] element of sku_arr)
         sku_btn = sku_arr[i][0].select('div.btn_addtocart')
 
+        # check that this item is wanted
         if i in WANTED_ITEMS or check_arg('--checkall') == True:
 
-            # check if the purchase_option is available
+            # check if available
             result = check_available(sku_btn, SKU_STRINGS[i])
 
             if result == True:
                 any_available = True
             
+            # error case
             if result == -1:
-                print("Error:", SKU_STRINGS[i], "returned unknown_status.")
-                #input("Press enter to continue.")
+                print("Error:", SKU_STRINGS[i], "returned unknown status.")
+                #input("press enter to continue.")
 
+            # choose correct string for writing to log file
             if result == True:
                 result_str = "IN STOCK."
             elif result == False:
@@ -130,49 +131,50 @@ def main():
             else:
                 result_str = "error! unknown status."
 
+            # open log file for writing (use append mode 'a')
             writefile = open(dirpath + os.sep + "indexVrResults.dat", "a")
 
-            writefile.write(f"[{now.date()}] [{str(now.time()).split('.')[0]}]" + " "\
+            # log file format can be found in README.md from the GitHub repository
+            writefile.write(f"[{now.date()}]" + " "\
+                            + f"[{str(now.time()).split('.')[0]}]" + " "\
                             + f"{items_checked+1}/{int(len(WANTED_ITEMS) if check_arg('--checkall') == False else len(SKU_STRINGS))}" + " "\
                             + "i=" + str(i) + " "\
                             + SKU_STRINGS[i] + " "\
                             + result_str\
                             + "\n")
 
+            # close file to free memory and resources
             writefile.close()
 
-            if result == True and check_arg('--noalarm') == False and alert_sound_1 != None:
+            if (result == True) and (check_arg('--noalarm') == False) and (alert_sound_1 != None):
 
                 # make loud sounds obnoxiously to alert the user!
                 print("ALARM ACTIVATING!")
                 for _ in range(30):
                     play_sound()
                     time.sleep(1)
-
-            elif alert_sound_1 == None:
-                #print("*Skipped alarm*")
-                pass
             
+            # increment counter
             items_checked += 1
 
-    # print newline, for nicer formatting in console :)
+    # print newline, for visual style
     print()
 
-    # if --nopause argument is present, don't pause before exit
+    # if --nopause argument is used, quit program now
     if check_arg('--nopause') == True:
 
-        # trying to spawn a new info message window (unfinished test...)
+        quit()
+
+        # failed test to create separate notification window (e.g. when script is running in background...)
+
         #if any_available or True:
             #play_sound()
             #subprocess.call(['python.exe', '-c', 'print(\'items are available!\');input(\'press enter to quit.\');'], creationflags=subprocess.CREATE_NEW_CONSOLE)
             #os.system("python.exe -c \"print('hello');input();\"")
             #pass
-
-        quit()
     else:
 
-        # if any wanted items are available, spawn a new visible console window and tell the user via print() text, then pause until the user closes it.
-        # (not implemented)
+        # maybe create notification window here, too
 
         if any_available:
             print("Items are available! Pausing application.\n")
@@ -181,10 +183,6 @@ def main():
         else:
             print("Nothing available. Sleeping for 3 seconds before exit.\n")
             time.sleep(3)
-
-    #play_sound()
-
-# === End of main function ===
 
 # === Program entry-point ===
 
@@ -197,6 +195,7 @@ if __name__ == "__main__":
     # it's useful to debug with play_sound() when running via pythonw.exe (no console window shown)
     #play_sound()
 
+    # call main functions
     main()
 
-# End of program entry-point, and End of program.
+# End of program.
